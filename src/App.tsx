7d094b17021691fe34aae79b70d9abcb6fa545f1
Kashyap Patel <kashyap.p@simformsolutions.com>
2025-07-11 15:43:12 +0530

    initial setup with tailwind css

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="text-center p-4 bg-blue-500 text-white">
+        <h1>Welcome to the Event Management App</h1>
+        <p>This is a simple React TypeScript application.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
